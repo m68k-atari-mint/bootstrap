0a4e595f3d92ce61f80a4162a0cf8800c2ad07cc
Miro Kropacek <miro.kropacek@gmail.com>
2019-05-30 09:48:15 +0200

    Small fixes

---
+++ enable_tun.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+chmod +s /usr/bin/aratapif
+
+modprobe tun
+chown $1 /dev/net/tun
+sleep 1
+tunctl -t tap0 -u $1
+sleep 1
+ifconfig tap0 192.168.251.1 pointopoint 192.168.251.2 netmask 255.255.255.255 up
+iptables -t nat -A POSTROUTING -s 192.168.251.2 -j MASQUERADE
+echo 1 > /proc/sys/net/ipv4/ip_forward
