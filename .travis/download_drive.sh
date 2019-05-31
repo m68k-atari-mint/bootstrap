387c2e5205d6f6bab6fc6fe4558bb0edfa62f08f
Miro Kropacek <miro.kropacek@gmail.com>
2019-05-31 17:16:28 +0200

    Fix download

--- .travis/download_drive.sh
+++ .travis/download_drive.sh
@@ -3,4 +3,4 @@
 # $1: stage
 # $2: drive letter
 
-wget -q --no-check-certificate -c "https://dl.bintray.com/m68k-atari-mint/opkg-bootstrap/images/${TRAVIS_COMMIT}/${1}/:drive_${2}.img.bz2" -O - | bzip2 -d
+wget -q --no-check-certificate -c "https://dl.bintray.com/m68k-atari-mint/opkg-bootstrap/images/${TRAVIS_COMMIT}/${1}/drive_${2}.img.bz2" -O - | bzip2 -d > drive_${2}.img
